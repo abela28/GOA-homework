11e6d3628a5c5830e0a937b5705a9eceffa90b05
abela28 <giorgi.abelashvili28@gmail.com>
2025-10-24 20:47:40 +0400

    1 homework

---
+++ Day1/level1.py
@@ -0,0 +1,24 @@
+#1) ცვლადი არის სპეციალური ადგილი მეხსიერებაში, სადაც ვინახავთ მონაცემებს.ცვლადები გვჭირდება იმისთვის,რომ პროგრამაში შევინახოთ, დავამუშავოთ და გამოვიყენოთ ინფორმაცია.
+
+# 2
+food = "shaurma<3"
+
+# 4)
+phone = "samsung"
+phone = "iphone"
+print(phone)
+
+# 5)
+name = "giorgi" 
+surename = "abelashvili"
+full_name = name + surename
+print(full_name)
+
+ # 6) ეს კოდი გამოიტანს სამჟრ დაწერილ "house" ს
+
+#  7)
+print("this is string")
+surename = "your surename"
+car = "mersedes"
+
+# 8) იმიტომ რომ print("lesson") წერია და კომპიუტერი აღიქავავს როგორც ტექსტური მონაცემი და არა როგორც ცვლადს
