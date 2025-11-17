61cd8b80ff3d19be8678ec637289bcb79562981b
abela28 <giorgi.abelashvili28@gmail.com>
2025-11-17 20:32:53 +0400

    homework7

---
+++ day8/homework7.py
@@ -0,0 +1,27 @@
+# 1
+age = int(input("enter your age: "))
+print(age//10)
+
+# 2
+num1 = int(input("random number: "))
+num2 = int(input("random number: "))
+print(num1*num2)
+
+# 3
+age = input("enter your age:")
+age = int(age)
+print(age)
+
+# 4
+num1 = int(input("random number: "))
+num2 = int(input("random number: "))
+num3 = int(input("random number: "))
+print((num1 + num2 + num3)/3)
+
+# 5
+birth_year = int(input("enter your year of birth: "))
+age = 2025 - birth_year
+print("your age is:", age)
+
+# p.s ბოლოში მძიმე არ გვისწავლია მაგრამ პლიუსი რომ ცავწერე ერორი ამომიგდო და გამახსენდა რომ წინა გაკვეთილზე მძიმე ისე ახსენეთ და მეთქი ვცდითქო 
+# და გამოვიდა და შემდეგ გამოვიკვლიე რას ნიშნავდა "," და გავიგე რო ის ყველაფრის შუაში წერს სპეისს არ აქვს მნიშვნელობა ტექსტი იქნება თუ რიცხვი<33
