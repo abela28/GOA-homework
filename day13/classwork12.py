c467334fbab0a751ba5bac893d612d18a9c45757
abela28 <giorgi.abelashvili28@gmail.com>
2026-01-02 17:10:36 +0400

    homework

---
+++ day13/classwork12.py
@@ -0,0 +1,35 @@
+# 1
+# for loop გამოიყენება მაშინ, როცა ვიცით რამდენჯერ უნდა შესრულდეს ციკლი
+# while loop მუშაობს მანამდე, სანამ პირობა Trueა
+
+# 2
+
+for i in range(10,39):
+    print(i)
+
+# 3
+i = 27
+while i >= 2:
+    print(i)
+    i -= 2
+
+
+# 4
+i = 100
+while i <= 500:
+    print(i)
+    i += 2
+
+# 5
+
+for i in "giorgi":
+    print(i)
+
+# 6
+
+while i>10:
+    print("its less")
+    i = i + 1
+
+# თუ i > 10, მაშინ დაიბეჭდება "its less" უსასრულოდ
+# რადგან i იზრდება და while პირობა არასდროს შეწყდება
---
+++ day13/homework13.py
@@ -0,0 +1,29 @@
+# # 1
+# for i in range(1,50,2):
+#     print(i)
+
+# # 2
+# name = input("enter your name: ")
+# for i in name:
+#     print(i)
+
+# # 3
+
+# for i in range(150):
+#     if i % 2 ==0:
+#         print("even")
+#     else:
+#         print("odd")
+
+# # 4
+# for i in range(20, -1,-1):
+#     print(i)
+
+# 5
+pas = "XqRzLpVbTn"
+while pas != input("enter pasword: "):
+     print("incorerect password!Try again:(")
+
+else:
+    print("password is correct!congratulation<33")
+
