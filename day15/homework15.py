65e8a9852201c4ed5e4aa207a68e741daf7f806a
abela28 <giorgi.abelashvili28@gmail.com>
2026-01-15 12:11:20 +0400

    homework and classwork

---
+++ day15/homework15.py
@@ -0,0 +1,43 @@
+# # 1
+# name = input("name:")
+
+# if name == "giorgi":
+#     print("Hello")
+# else:
+#     print("Bye")
+
+# # 2
+# M_num = 14
+# Y_num = int(input("number:"))
+
+# if M_num == Y_num:
+#     print("Perfect")
+# elif M_num < Y_num:
+#     print("More")
+# else:
+#     print("Less")
+
+# # 3
+# NAME = input("name:")
+# AGE = int(input("age:"))
+
+# if NAME == "giorgi"and AGE == 14:
+#     print("Twinse")
+# else:
+#     print("not twins")
+
+# # 4
+# NUM = int(input("Number:"))
+
+# if NUM % 3 and NUM % 5:
+#     print("არის 3-ის და 5-ის ჯერადი")
+# else:
+#     print("არ არის 3-ის და 5-ის ჯერადი")
+
+# 5
+pas = input("password: ")
+
+if len(pas) < 8:
+    print("Bad password")
+else:
+    print("Great password")
