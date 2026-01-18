a724905648c9382fd41c4a2f0fd60681411256b3
abela28 <giorgi.abelashvili28@gmail.com>
2026-01-18 20:09:53 +0400

    home and class works'

--- day15/homework15.py
+++ day15/homework15.py
@@ -1,38 +1,38 @@
-# # 1
-# name = input("name:")
+# 1
+name = input("name:")
 
-# if name == "giorgi":
-#     print("Hello")
-# else:
-#     print("Bye")
+if name == "giorgi":
+    print("Hello")
+else:
+    print("Bye")
 
-# # 2
-# M_num = 14
-# Y_num = int(input("number:"))
+# 2
+M_num = 14
+Y_num = int(input("number:"))
 
-# if M_num == Y_num:
-#     print("Perfect")
-# elif M_num < Y_num:
-#     print("More")
-# else:
-#     print("Less")
+if M_num == Y_num:
+    print("Perfect")
+elif M_num < Y_num:
+    print("More")
+else:
+    print("Less")
 
-# # 3
-# NAME = input("name:")
-# AGE = int(input("age:"))
+# 3
+NAME = input("name:")
+AGE = int(input("age:"))
 
-# if NAME == "giorgi"and AGE == 14:
-#     print("Twinse")
-# else:
-#     print("not twins")
+if NAME == "giorgi"and AGE == 14:
+    print("Twinse")
+else:
+    print("not twins")
 
-# # 4
-# NUM = int(input("Number:"))
+# 4
+NUM = int(input("Number:"))
 
-# if NUM % 3 and NUM % 5:
-#     print("არის 3-ის და 5-ის ჯერადი")
-# else:
-#     print("არ არის 3-ის და 5-ის ჯერადი")
+if NUM % 3 and NUM % 5:
+    print("არის 3-ის და 5-ის ჯერადი")
+else:
+    print("არ არის 3-ის და 5-ის ჯერადი")
 
 # 5
 pas = input("password: ")
